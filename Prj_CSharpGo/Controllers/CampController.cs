406ad5d10eab41e092862b531bde6cc859196456
Jay <saber83528@gmail.com>
2021-07-27 02:40:40 +0800

    更新

--- Prj_CSharpGo/Controllers/CampController.cs
+++ Prj_CSharpGo/Controllers/CampController.cs
@@ -25,22 +25,5 @@ namespace Prj_CSharpGo.Controllers
 
             return View(campList);
         }
-
-        public IActionResult grass()
-        {
-         
-            return View();
-        }
-
-        public IActionResult wooden()
-        {
-            return View();
-        }
-
-        public IActionResult stone()
-        {
-            return View();
-        }
-
     }
 }
