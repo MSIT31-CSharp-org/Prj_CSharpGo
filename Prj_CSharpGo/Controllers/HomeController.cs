528fb3a2708be173d523bb2e7babeab98b422791
Jay <saber83528@gmail.com>
2021-07-27 02:40:40 +0800

    更新

--- Prj_CSharpGo/Controllers/HomeController.cs
+++ Prj_CSharpGo/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Prj_CSharpGo.Models;
 using System;
@@ -15,7 +14,6 @@ namespace Prj_CSharpGo.Controllers
         private readonly ILogger<HomeController> _logger;
         private WildnessCampingContext _context;
 
-        //測試喔~~~
         public HomeController(ILogger<HomeController> logger , WildnessCampingContext dbContext)
         {
             _logger = logger;
@@ -36,37 +34,6 @@ namespace Prj_CSharpGo.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Login(string userName, string password)
-        {
-            if (string.IsNullOrEmpty(userName))
-            {
-                return View();
-            }
-            // Session 的寫入
-            HttpContext.Session.SetString("userName", userName);
-            return Redirect("/Home/Index");
-        }
-
-        public IActionResult MemberCenter()
-        {
-            string userName = HttpContext.Session.GetString("userName") ?? "Guest";
-            if (userName == "Guest")
-            {
-                return Redirect("/Home/Index");
-            }
-            return View();
-        }
-        public IActionResult Logout()
-        {
-            HttpContext.Session.Remove("userName");
-            return Redirect("/Home/Index");
         }
     }
 }
