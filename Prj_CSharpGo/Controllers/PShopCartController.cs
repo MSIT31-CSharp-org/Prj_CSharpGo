5043e6d4fa04cc7ccda70b035762db517351fd32
Rachel1993 <somesaylove199346@gmail.com>
2021-07-28 00:19:50 +0800

    Rachel0728

---
+++ Prj_CSharpGo/Controllers/PShopCartController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Prj_CSharpGo.Models;
+using Newtonsoft.Json;
+
+namespace Prj_CSharpGo.Controllers
+{
+    public class PShopCartController : Controller
+    {
+        private readonly WildnessCampingContext _context;
+        public PShopCartController(WildnessCampingContext context)
+        {
+            _context = context;
+        }
+
+        //public IActionResult Index()//從商品庫抓出所有商品
+        // {
+        // List<Product> Products = _context.Products.ToList();
+        // return View(Products);
+        //}
+
+        //從購物車資料庫抓出所有東西
+         public IActionResult Index()//從商品庫抓出所有商品
+         {
+         List<ShoppingCart> ShopProductsList = _context.ShoppingCarts.ToList();
+            
+         return View(ShopProductsList);
+         }
+
+       //在購物車頁面點那個商品的圖片會回到商品詳細介紹那一頁
+        public IActionResult Detail(int? ProductId)
+        {
+
+            Product reProduct = _context.ShoppingCarts.Find(ProductId) ;
+
+            ReturnIndexModels returnIndexModels = new ReturnIndexModels();
+            returnIndexModels.userFilterModel = new CampreserveUserFilterModel()
+            {
+                CampId = id,
+                StartDate = post["StartDay"],
+                EndDate = post["EndDay"],
+                CampQuantity = Quantity
+            };
+
+            reProduct
+           return View(reProduct);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Detail()
+        {
+            var shquery = from o in _context.Products
+                        orderby o.CategoryId ascending, o.Price descending
+                        select o;
+            return await shquery.ToListAsync();
+        }
+}
