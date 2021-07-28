58f11b55ebd4c7a8cde493a60c435407d6c2a0f4
Jay <saber83528@gmail.com>
2021-07-28 08:28:48 +0800

    更新

--- Prj_CSharpGo/Models/ViewModels/EmployeeOrder.cs
+++ Prj_CSharpGo/Models/ViewModels/EmployeeOrder.cs
@@ -7,10 +7,8 @@ namespace Prj_CSharpGo.Models.ViewModels
 {
     public class EmployeeOrder
     {
-
         public Order _order { get; set; }
         public IEnumerable<OrderDetail> OrderDetails { get; set; }
         public IEnumerable<Product> Products { get; set; }
-
     }
 }
