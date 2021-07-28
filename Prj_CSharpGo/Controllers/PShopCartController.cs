aee46ef661df7ead6f04eb3fe900324236cc6a11
Rachel1993 <somesaylove199346@gmail.com>
2021-07-28 18:13:22 +0800

    1100728RC更

--- Prj_CSharpGo/Controllers/PShopCartController.cs
+++ Prj_CSharpGo/Controllers/PShopCartController.cs
@@ -5,56 +5,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Prj_CSharpGo.Models;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+using Prj_CSharpGo.Models.ShopCartViewModels;
+
 
 namespace Prj_CSharpGo.Controllers
 {
     public class PShopCartController : Controller
     {
+        private readonly ILogger<CampreserveController> _logger;
         private readonly WildnessCampingContext _context;
-        public PShopCartController(WildnessCampingContext context)
+        public PShopCartController(WildnessCampingContext context, ILogger<CampreserveController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-        //public IActionResult Index()//從商品庫抓出所有商品
-        // {
-        // List<Product> Products = _context.Products.ToList();
-        // return View(Products);
-        //}
+
 
         //從購物車資料庫抓出所有東西
-         public IActionResult Index()//從商品庫抓出所有商品
-         {
-         List<ShoppingCart> ShopProductsList = _context.ShoppingCarts.ToList();
-            
-         return View(ShopProductsList);
-         }
-
-       //在購物車頁面點那個商品的圖片會回到商品詳細介紹那一頁
-        public IActionResult Detail(int? ProductId)
+        public IActionResult Index()//從商品庫抓出所有商品
         {
+            List<ShoppingCart> ShopProductsList = _context.ShoppingCarts.ToList();
 
-            Product reProduct = _context.ShoppingCarts.Find(ProductId) ;
-
-            ReturnIndexModels returnIndexModels = new ReturnIndexModels();
-            returnIndexModels.userFilterModel = new CampreserveUserFilterModel()
-            {
-                CampId = id,
-                StartDate = post["StartDay"],
-                EndDate = post["EndDay"],
-                CampQuantity = Quantity
-            };
-
-            reProduct
-           return View(reProduct);
+            return View(ShopProductsList);
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> Detail()
-        {
-            var shquery = from o in _context.Products
-                        orderby o.CategoryId ascending, o.Price descending
-                        select o;
-            return await shquery.ToListAsync();
-        }
+        //在購物車頁面點那個商品的圖片會回到商品詳細介紹那一頁
+        // public IActionResult Detail(int? ProductId)
+        // {
+
+        // Product reProduct = _context.ShoppingCarts.Find(ProductId) ;
+
+        // ReturnIndexModels returnIndexModels = new ReturnIndexModels();
+        // returnIndexModels.userFilterModel = new CampreserveUserFilterModel()
+        // {
+        //  CampId = id,
+        // StartDate = post["StartDay"],
+        // EndDate = post["EndDay"],
+        //  CampQuantity = Quantity
+        // };
+
+        //reProduct
+        // return View(reProduct);
+    }
+
+    //[HttpGet]
+    // public async Task<ActionResult<IEnumerable<Product>>> Detail()
+    // {
+    //  var shquery = from o in _context.Products
+    //  orderby o.CategoryId ascending, o.Price descending
+    //select o;
+    //return await shquery.ToListAsync();
+    // }
 }
---
+++ Prj_CSharpGo/Models/ShopCartViewModels/PShopCartViewMolds.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prj_CSharpGo.Models.ShopCartViewModels
+{
+    public class PShopCartViewMolds
+    {
+        public Order order { get; set; }
+
+        public IEnumerable<Product> Product { get; set; }
+
+        public IEnumerable<User> User { get; set; }
+        public IEnumerable<ShoppingCart> ShoppingCart { get; set; }
+
+        public int? SMTotal { get; set; }
+
+        public int? BigTotal { get; set; }
+
+    }
+}
