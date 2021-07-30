c797af3797de03bb6bfb56a9635444b757fc2ed9
yujanee <chuan60085@gmail.com>
2021-07-30 23:50:34 +0800

    products、productslist更新

--- Prj_CSharpGo/Controllers/HomeController.cs
+++ Prj_CSharpGo/Controllers/HomeController.cs
@@ -36,20 +36,37 @@ namespace Prj_CSharpGo.Controllers
         }
 
         // 正常來說會收到一段string productid 但是目前頁面還沒處理好 我就直接給 productid = "Aa10CL007"
-        public IActionResult Product(string productid)
+        public IActionResult Product(string productid, string categoryid)
         {
 
             productid = "Aa10CL007";
+            categoryid = "A";
 
             // 新創個類別 類別在~/Model/ViewModels/ProductHome.cs 修改類別的話Jane DiDI要討論一下 建議是從下面{}取得的值修改就好
             ProductHome productHome = new ProductHome
             {
                 products = from o in _context.Products
-                           where o.ProductId == productid
+                           where o.ProductId == productid 
                            select o,
                 productImgs = from o in _context.ProductImgs
                               where o.ProductId == productid
                               select o ,
+                categories = from o in _context.Categories
+                             where o.CategoryId == categoryid
+                             select o,
+                categoriesTypeIs = _context.CategoriesTypeIs.ToList(),
+                categoriesTypeIis = _context.CategoriesTypeIis.ToList()
+            };
+            return View(productHome);
+        }
+
+        public IActionResult ProductsList()
+        {
+            
+            ProductHome productHome = new ProductHome
+            {
+                products = _context.Products.ToList(),
+                productImgs = _context.ProductImgs.ToList(),
                 categories = _context.Categories.ToList(),
                 categoriesTypeIs = _context.CategoriesTypeIs.ToList(),
                 categoriesTypeIis = _context.CategoriesTypeIis.ToList()
