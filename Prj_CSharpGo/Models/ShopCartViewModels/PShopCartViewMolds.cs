ad6a016a7b03981760b59f198de8c05584b541b2
Rachel1993 <somesaylove199346@gmail.com>
2021-08-02 05:39:11 +0800

    123

--- Prj_CSharpGo/Models/ShopCartViewModels/PShopCartViewMolds.cs
+++ Prj_CSharpGo/Models/ShopCartViewModels/PShopCartViewMolds.cs
@@ -11,18 +11,20 @@ namespace Prj_CSharpGo.Models.ShopCartViewModels
         public List<Product> Products { get; set; }
         public List<User> Users { get; set; }
         public List<ShoppingCart> ShoppingCarts { get; set; }
+        public ShoppingCartUserFilterModel PuserFilterModel { get; set; }
+    }
+
+    public class ShoppingCartUserFilterModel
+    {
+        public int UserId { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int? Quantity { get; set; }
+        public int? UnitPrice { get; set; }
+        public int? SMTotal { get; set; }
+        public int? BigTotal { get; set; }
 
     }
 
 
-
-    //public class ShowAllshCart {
-
-    //    public Order order { get; set; }
-
-    //    public int? SMTotal { get; set; }
-
-    //    public int? BigTotal { get; set; }
-    //    }
-    
 }
