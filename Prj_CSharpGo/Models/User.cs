7e3a6356de236a21b726d1b1b8fcf3a225ea1f17
Jay <saber83528@gmail.com>
2021-08-04 13:21:11 +0800

    123

--- Prj_CSharpGo/Models/User.cs
+++ Prj_CSharpGo/Models/User.cs
@@ -16,19 +16,19 @@ namespace Prj_CSharpGo.Models
         }
 
         public int UserId { get; set; }
-        public string UserAccount { get; set; }
+        public string Email { get; set; }
         public string UserPassword { get; set; }
+        public string ConfirmPassword { get; set; }
         public string UserName { get; set; }
         public string Birthday { get; set; }
         public string Region { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
-        public string Email { get; set; }
         public string Img { get; set; }
         public string DiscountCode { get; set; }
         public DateTime? UpdateDate { get; set; }
         public string UserStatus { get; set; }
-        public string Vip { get; set; }
+        public bool? IsSuccess { get; set; }
 
         public virtual ICollection<CampOrder> CampOrders { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
