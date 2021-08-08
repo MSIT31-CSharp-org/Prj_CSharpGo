528576629728cb523ac60050d9e523311ba885c3
Jay <saber83528@gmail.com>
2021-08-08 20:30:14 +0800

    更新

--- Prj_CSharpGo/Models/Order.cs
+++ Prj_CSharpGo/Models/Order.cs
@@ -18,6 +18,7 @@ namespace Prj_CSharpGo.Models
         public string PayMethod { get; set; }
         public DateTime? OrderDate { get; set; }
         public string Approval { get; set; }
+        public string Address { get; set; }
 
         public virtual User User { get; set; }
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
