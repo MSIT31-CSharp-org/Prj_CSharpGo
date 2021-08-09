cf68e535e3895e9c88e4609b1c18872538d2eae4
Steven <stevenfengwei@gmail.com>
2021-08-10 04:45:09 +0800

    Ｆｉｘ＿Ａｇａｉｎ

--- Prj_CSharpGo/Controllers/AuthController.cs
+++ Prj_CSharpGo/Controllers/AuthController.cs
@@ -189,6 +189,7 @@ namespace Prj_CSharpGo.Controllers
         }
 
         // 會員中心 => 訂單變更
+
         public async Task<IActionResult> MemberOrderEdit(int? id)
         {
             string userId = HttpContext.Session.GetString("userId") ?? "Guest";
@@ -203,11 +204,13 @@ namespace Prj_CSharpGo.Controllers
                 return Redirect("/Auth/Login");
             }
 
-            var MemberOrderDetailView = _context.OrderDetails.Where(o => o.OrderId == id).ToList()[0];
+            // 
+            var MemberOrderDetailView = _context.Orders.Where(o => o.OrderId == id).ToList()[0];
 
             var CancelOrder = (from u in _context.OrderDetails
-                               where u.OrderId == MemberOrderDetailView.OrderId
+                               where u.OrderId == id
                                select u.OrderId).ToList()[0];
+
             var f_order_user = (from u in _context.Users
                                 where u.UserId == userID.UserId
                                 select u.UserId).ToList()[0];
@@ -218,7 +221,6 @@ namespace Prj_CSharpGo.Controllers
                 HttpContext.Session.SetString("userToastr", " SoS！顯示異常");
                 return View();
             }
-
             MemberOrder User_order = new MemberOrder()
             {
                 _order = await _context.Orders.FindAsync(id),
@@ -226,17 +228,12 @@ namespace Prj_CSharpGo.Controllers
                 Products = await _context.Products.ToListAsync(),
                 //Users = _context.Users.Where(u => u.UserId == f_order_user)
             };
-
             if (User_order == null)
             {
                 return NotFound();
             }
-
             return View(User_order);
-
         }
-
-
 
         // =================================================================================================================================================================================
         // 【POST : 登入 / 註冊】 ===========================================================================================================================================================
@@ -531,11 +528,11 @@ namespace Prj_CSharpGo.Controllers
                 return View();
             }
 
-            User member = new User()
-            {
-                UserPassword = NewPassword,
-                ConfirmPassword = NewPassword
-            };
+            //User member = new User()
+            //{
+            //    UserPassword = NewPassword,
+            //    ConfirmPassword = NewPassword
+            //};
 
             var changUserNewPwd = _context.Users.Find(f_userId);
             changUserNewPwd.UserPassword = NewPassword;
@@ -549,13 +546,9 @@ namespace Prj_CSharpGo.Controllers
 
         // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++【 會員中心 => 訂單變更 】+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         [HttpPost]
-        public async Task<IActionResult> MemberOrderEdit(int id,[Bind("OrderId,UserId,OrderDate,PayMethod,TotalPrice,Approval,Address,UserName")] Order order)
+        //public async Task<IActionResult> MemberOrderEdit(int id,[Bind("OrderId,UserId,OrderDate,PayMethod,TotalPrice,Approval,Address,UserName")] Order order)
+        public async Task<IActionResult> MemberOrderEdit(int OrderId)
         {
-            //if (id != orderDetail.OrderId)
-            //{
-            //    return NotFound();
-            //}
-
             string userId = HttpContext.Session.GetString("userId") ?? "Guest";
 
             // 找出目前已登入的使用者 id
@@ -568,26 +561,48 @@ namespace Prj_CSharpGo.Controllers
                 return Redirect("/Auth/Login");
             }
 
+            //var f_order_user = (from u in _context.Users
+            //                    where u.UserId == userID.UserId
+            //                    select u.UserId).ToList()[0];
 
-            var CancelOrder = (from u in _context.OrderDetails
-                               where u.OrderId == id
-                               where u.Approval == order.Approval
-                               select u.OrderId).FirstOrDefault();
+            var CancelOrder = _context.Orders.Where(o => o.OrderId == OrderId).Select(o => o.OrderId).FirstOrDefault();
 
-            var f_order_user = (from u in _context.Users
-                                where u.UserId == userID.UserId
-                                select u.UserId).ToList()[0];
+            var fOrderId = _context.Orders.Find(CancelOrder);
+            fOrderId.OrderId = fOrderId.OrderId;
+            fOrderId.UserId = fOrderId.UserId;
+            fOrderId.TotalPrice = fOrderId.TotalPrice;
+            fOrderId.PayMethod = fOrderId.PayMethod;
+            fOrderId.OrderDate = fOrderId.OrderDate;
+            fOrderId.Approval = "WL";
+            fOrderId.Address = fOrderId.Address;
+            fOrderId.UserName = fOrderId.UserName;
 
-            // 設定該使用者之 UserId 用以變更
-            var CancelOrderinfo = _context.Orders.Find(CancelOrder);
-            CancelOrderinfo.Approval = "WL";   // 將訂單狀態變更為"取消" = "WL"
+            _context.Update(fOrderId);
+            _context.SaveChanges();
 
-            _context.Update(CancelOrderinfo);
-            await _context.SaveChangesAsync();
-
+            // 因為View引用的是MemberViewModel類別，最後傳回View的也必須是MemberViewModel類別，否則會是Null  ==> 
+            MemberOrder User_order = new MemberOrder()
+            {
+                _order =  _context.Orders.Find(fOrderId.OrderId),
+                OrderDetails = _context.OrderDetails.Where(o => o.OrderId == fOrderId.OrderId),
+                Products =  _context.Products.ToList(),
+            };
+            // 訂單狀態變更成功，等待1.5秒轉導至登入頁 ==============================================================================
+            CancellationTokenSource cts = new CancellationTokenSource();
+            try
+            {
+                await Task.Delay(1500, cts.Token);
+            }
+            catch (TaskCanceledException ex)
+            {
+                string Status = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff ==>") + ex.ToString();
+                HttpContext.Session.SetString("userToastr", "很抱歉！您沒有權限變更此訂單");
+                return View();
+            }
+            // ===================================================================================================================
             HttpContext.Session.SetString("userToastr", "已為您完成取消訂單");
-            HttpContext.Session.SetString("userId", f_order_user.ToString());
-            return RedirectToAction(nameof(MemberOrder));
+            HttpContext.Session.SetString("userId", userId);
+            return View(User_order);
         }
     }
 }
--- Prj_CSharpGo/Models/ViewModels/MemberOrder.cs
+++ Prj_CSharpGo/Models/ViewModels/MemberOrder.cs
@@ -8,12 +8,45 @@ namespace Prj_CSharpGo.Models.ViewModels
     public class MemberOrder
     {
 
+        //public IEnumerable<User> Users { get; set; }
         public Order _order { get; set; }
         public IEnumerable<OrderDetail> OrderDetails { get; set; }
         public IEnumerable<Product> Products { get; set; }
 
 
-        //public IEnumerable<User> Users { get; set; }
+        //// Order 
+        //public int OrderId { get; set; }
+        //public int UserId { get; set; }
+        //public int? TotalPrice { get; set; }
+        //public string PayMethod { get; set; }
+        //public DateTime? OrderDate { get; set; }
+        //public string Approval { get; set; }
+        //public string Address { get; set; }
+        //public string UserName { get; set; }
 
+
+        //// OderDetail
+        ////public int OrderId { get; set; }
+        //public string ProductId { get; set; }
+        //public int? UnitPrice { get; set; }
+        //public short? Quantity { get; set; }
+        //public double? Discount { get; set; }
+        //public string Commets { get; set; }
+        ////public string Approval { get; set; }
+        //public int Odpk { get; set; }
+
+
+        //// Product
+        ////public string ProductId { get; set; }
+        //public string CategoryId { get; set; }
+        //public string ProductName { get; set; }
+        //public string ProductDescription { get; set; }
+        //public string Specification { get; set; }
+        //public int? Cost { get; set; }
+        ////public int? UnitPrice { get; set; }
+        //public short? UnitInStock { get; set; }
+        //public string Status { get; set; }
+        ////public string Approval { get; set; }
+        //public string CategoryType { get; set; }
     }
 }
